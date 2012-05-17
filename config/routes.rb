1275318d23c9fbf1024f556bfe06239ad3ee80e5
Steve Tipton <steve.tipton@employmentguide.com>
2012-05-17 19:54:00 -0400

    add games routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Wishbone::Application.routes.draw do
+
+  scope controller: 'games', constraints: { id: /[0-9]+/ } do
+    match '/schedule' => :index, as: :schedule
+    match '/games/:id' => :show, as: :game
+  end
 end
