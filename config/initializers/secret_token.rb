a3e493c4fbc6c0fb3e482f9ca55d4b52b189cc15
Steve Tipton <steve.tipton@employmentguide.com>
2012-05-17 08:42:51 -0400

    initial commit for new rails app: wishbone

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wishbone::Application.config.secret_token = '3c0e7c9103ef65c7f9389c0f92f9d6ed80fce676ff9917b1c1e1220eef8acfc306201aa286f177dec19324284df59abe23e9c085f1c203ed883d664c4860a811'
