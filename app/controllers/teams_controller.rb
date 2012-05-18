6658f871a54978087c2cf437663952840a802fa6
Steve Tipton <steve.tipton@employmentguide.com>
2012-05-18 10:51:00 -0400

    add assigns and passing teams controller specs

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,12 @@
 class TeamsController < ApplicationController
+
   def index
+    Wildcat::Team.all { |team| @teams = team }
+    Wildcat::Config.hydra.run
   end
 
   def show
+    Wildcat::Team.find(params[:id]) { |team| @team = team }
+    Wildcat::Config.hydra.run
   end
 end
