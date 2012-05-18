6a7828b684fff02aab66a532793fbf6651fe271d
Steve Tipton <steve.tipton@employmentguide.com>
2012-05-18 09:24:35 -0400

    initial team_games controller spec with two passing examples

--- spec/controllers/team_games_controller_spec.rb
+++ spec/controllers/team_games_controller_spec.rb
@@ -2,11 +2,16 @@ require 'spec_helper'
 
 describe TeamGamesController do
 
+  before do
+    @team = FactoryGirl.build(:team)
+  end
+
   describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
+
+    before { get :index, id: @team.id }
+
+    it "should be successful" do
       response.should be_success
     end
   end
-
 end
