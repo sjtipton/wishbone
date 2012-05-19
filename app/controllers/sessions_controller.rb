cbc7ac4ea6dfc8da526ca80c47710e024389119b
Steve Tipton <steve.tipton@employmentguide.com>
2012-05-18 23:49:04 -0400

    passing sessions controller spec

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,17 @@
 class SessionsController < ApplicationController
 
   def authenticate_user
+    auth_hash = request.env['omniauth.auth']
+
+    unless auth_hash.present?
+      redirect_to request.env['omniauth.origin'] || root_path,
+               status: 301,
+                alert: "Sorry, we were not able to authenticate you using your chosen sign on method" and return
+    end
+
+    session[:user] = { info: auth_hash['info'],
+                        uid: auth_hash['uid'],
+                   provider: auth_hash['provider'] }
+    redirect_to root_path, status: 301, notice: "You have successfully been authenticated through Facebook" and return
   end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -5,36 +5,84 @@ describe SessionsController do
 
   describe "POST 'authenticate_user'" do
 
-    context "when authenticating though Facebook" do
+    context "when successully authenticating though Facebook" do
 
       before do
-        request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
+        request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:facebook]
         post :authenticate_user, provider: 'facebook'
       end
 
-      it "should be successful" do
-        response.should be_success
-      end
-
-      it "should set user's email" do
-        assigns(:user_data)[:info][:email].should == request.env["omniauth.auth"]["info"]["email"]
-      end
-
-      it "should set user's first name" do
-        assigns(:user_data)[:info][:first_name].should == request.env["omniauth.auth"]["info"]["first_name"]
-      end
-
-      it "should set user's last name" do
-        assigns(:user_data)[:info][:last_name].should == request.env["omniauth.auth"]["info"]["last_name"]
-      end
-
-      it "should set user's name" do
-        assigns(:user_data)[:info][:name].should == request.env["omniauth.auth"]["info"]["name"]
+      it "should be permanent redirect" do
+        response.status.should be(301)
       end
 
       it "should instantiate a user session" do
-	    	request.session[:user].should_not be_nil
-	    end
+        request.session[:user].should_not be_nil
+        request.session[:user][:uid].should_not be_nil
+        request.session[:user][:provider].should eq 'facebook'
+        request.session[:user][:info]['email'].should eq request.env['omniauth.auth']['info']['email']
+        request.session[:user][:info]['first_name'].should eq request.env['omniauth.auth']['info']['first_name']
+        request.session[:user][:info]['last_name'].should eq request.env['omniauth.auth']['info']['last_name']
+        request.session[:user][:info]['name'].should eq request.env['omniauth.auth']['info']['name']
+      end
+
+      it "should redirect to root_path" do
+        response.should redirect_to root_path
+      end
+
+      it "should set flash[:notice] as 'You have successfully been authenticated through Facebook'" do
+        flash[:notice].should eq 'You have successfully been authenticated through Facebook'
+      end
+    end
+
+    context "when declining access or otherwise not valid " do
+
+      context "when the omniauth origin is present" do
+
+        before do
+          request.env['omniauth.origin'] = schedule_path
+          post :authenticate_user, provider: 'facebook'
+        end
+
+        it "should be permanent redirect" do
+          response.status.should be(301)
+        end
+
+        it "should not instantiate a user session" do
+          request.session[:user].should be_nil
+        end
+
+        it "should set flash[:alert] as 'Sorry, we were not able to authenticate you using your chosen sign on method'" do
+          flash[:alert].should eq 'Sorry, we were not able to authenticate you using your chosen sign on method'
+        end
+
+        it "should redirect to request.env['omniauth.origin']" do
+          response.should redirect_to request.env['omniauth.origin']
+        end
+      end
+
+      context "when the omniauth origin is nil" do
+
+        before do
+          post :authenticate_user, provider: 'facebook'
+        end
+
+        it "should be permanent redirect" do
+          response.status.should be(301)
+        end
+
+        it "should not instantiate a user session" do
+          request.session[:user].should be_nil
+        end
+
+        it "should set flash[:alert] as 'Sorry, we were not able to authenticate you using your chosen sign on method'" do
+          flash[:alert].should eq 'Sorry, we were not able to authenticate you using your chosen sign on method'
+        end
+
+        it "should redirect to root_path" do
+          response.should redirect_to root_path
+        end
+      end
     end
   end
 end
