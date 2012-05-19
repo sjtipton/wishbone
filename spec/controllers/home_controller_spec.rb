e4f4e10c3c4a89d416d8731a876997d1c2a81e53
Steve Tipton <steve.tipton@employmentguide.com>
2012-05-18 22:47:28 -0400

    add initial home controller spec and controller

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
 describe HomeController do
+  render_views
 
   describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
+
+    before { get :index }
+
+    it "should be successful" do
       response.should be_success
     end
-  end
 
+    it "should render 'home/index'" do
+      response.should render_template('home/index')
+    end
+  end
 end
