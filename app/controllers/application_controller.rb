386fba0eb661e800a7ddf181e4ca3cd377e346cd
Steve Tipton <steve.tipton@employmentguide.com>
2012-05-19 01:22:25 -0400

    fix helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method [ :authenticate_user_with_facebook!,
+                  :current_facebook_user,
+                  :facebook_user_signed_in? ]
 
   private
 
   def authenticate_user_with_facebook!
-    redirect_to authentication_menu_path unless current_facebook_user.present?
+    redirect_to authentication_menu_path unless facebook_user_signed_in?
   end
 
   def current_facebook_user
     session[:user]
+  end
+
+  def facebook_user_signed_in?
+    current_facebook_user.present?
   end
 end
