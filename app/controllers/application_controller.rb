bf3d3d7e8081fc199f67139e68e7aa3da7c71af6
Steve Tipton <steve.tipton@employmentguide.com>
2012-05-19 00:45:47 -0400

    add application methods for session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  def authenticate_user_with_facebook!
+    redirect_to authentication_menu_path unless current_facebook_user.present?
+  end
+
+  def current_facebook_user
+    session[:user]
+  end
 end
