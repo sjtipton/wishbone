ebbc95daba5f9a6522e62b2efc15f19eb580e1e3
Steve Tipton <steve.tipton@employmentguide.com>
2012-05-22 19:38:27 -0400

    add user_id and forecast_id to predictions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120522232752) do
+ActiveRecord::Schema.define(:version => 20120522233607) do
 
   create_table "forecasts", :force => true do |t|
     t.string   "title",      :null => false
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(:version => 20120522232752) do
     t.integer  "losing_team_score"
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
+    t.integer  "user_id",            :null => false
+    t.integer  "forecast_id",        :null => false
   end
 
   create_table "users", :force => true do |t|
