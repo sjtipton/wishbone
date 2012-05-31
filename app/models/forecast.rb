af97cbcee575e48018be6b33e3a065cf2558b73b
Steve Tipton <steve.tipton@employmentguide.com>
2012-05-31 10:35:15 -0400

    hack in progress

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -2,7 +2,9 @@ class Forecast < ActiveRecord::Base
   has_many :predictions, dependent: :destroy
   belongs_to :user
 
-  attr_accessible :title, :user_id
+  accepts_nested_attributes_for :predictions
+
+  attr_accessible :title, :user_id, :predictions_attributes
 
   validates :title, :user_id, presence: true
   validates :user_id, numericality: { greater_than: 0 }
